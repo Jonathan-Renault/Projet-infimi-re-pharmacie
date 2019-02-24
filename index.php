a3a20d0790e7e38e0c38e8335396e2297581303d
jonre <jon.renault@protonmail.com>
2019-02-24 11:08:24 +0100

    merge

--- index.php
+++ index.php
@@ -1,106 +1,79 @@
 <?php
 
 require 'assets/inc/pdo.php';
-require 'assets/inc/function.php';
+require 'assets/inc/fonctions.php';
 require 'assets/inc/request.php';
 
-include "assets/inc/header.php";
+
 
 if (!empty($_SESSION['nom'])){
-    header("Location: mesureBas.php");
+    header("Location: form_chaussette.php");
 }
 
-if (!empty($_POST['user_name']) and !empty($_POST['user_mdp'])){
+if (!empty($_POST['user_name']) and !empty($_POST['user_mdp'])) {
 
     $name = strtolower(trim(strip_tags($_POST['user_name'])));
     $mdp = trim(strip_tags($_POST['user_mdp']));
 
     $result = selectbyname($name);
 
-include "assets/inc/header.php";
-?>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-2"></div>
 
-            <div class="col-md-8" style="margin-bottom: 30vh;">
-
-                <h2>Connexion</h2>
-
-                <form>
-                    <div class="form-group">
-                        <label for="">Nom :</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="">Mot de passe :</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1">
-                    </div>
-
-                    <button class="btn btn-primary" type="submit">Valider</button>
-
-                </form>
-
-            </div>
-            <div class="col-md-2">
-            </div>
-        </div>
-    </div>
-
-
-    if (strlen($name) <= 1){
+    if (strlen($name) <= 1) {
         $erreur = 'Nom trop court';
-    }
-    else if (strlen($name) > 50){
+    } else if (strlen($name) > 50) {
         $erreur = 'Nom trop long';
     }
-    if (strlen($mdp) < 5){
+    if (strlen($mdp) < 5) {
         $erreur = 'Mot de passe trop court';
-    }
-    else if (strlen($mdp) > 255){
+    } else if (strlen($mdp) > 255) {
         $erreur = 'Mot de passe trop long';
     }
 
     if (!empty($result)) {
-        if (password_verify($mdp,$result['mdp'])) {
+        if (password_verify($mdp, $result['mdp'])) {
 
-                $_SESSION['nom'] = $result['nom'];
-                $_SESSION['prenom'] = $result['prenom'];
-                $_SESSION['status'] = $result['status'];
-                $_SESSION['mdp_modif'] = $result['mdp_modif'];
+            $_SESSION['nom'] = $result['nom'];
+            $_SESSION['prenom'] = $result['prenom'];
+            $_SESSION['status'] = $result['status'];
+            $_SESSION['mdp_modif'] = $result['mdp_modif'];
 
-            if ($result['mdp_modif'] == 'no'){
+            if ($result['mdp_modif'] == 'no') {
                 header("Location: nouveau_mdp.php");
-            }
-            else {
+            } else {
                 header("Location: mesureBas.php");
             }
-        }
-        else {
+        } else {
             echo 'Le mot de passe est invalide.';
         }
-    }
-    else {
+    } else {
         echo 'Le nom saisi est invalide.';
     }
+
 }
 
-
+include "assets/inc/header.php";
 ?>
 
-<form action="index.php" method="post">
-    <div>
-        <label for="name">Nom :</label><br/>
-        <input type="text" id="name" name="user_name" required>
-    </div>
-    <div>
-        <label for="mdp">Mot de passe :</label><br/>
-        <input type="password" id="mdp" name="user_mdp" required>
-    </div>
-    <?php if (!empty($erreur)) {echo $erreur;} ?>
-    <input type="submit" value="Connexion">
-</form>
+                <h2>Connexion</h2>
+
+                <form action="index.php" method="post">
+
+                <div class="form-group">
+                        <label for="name">Nom :</label><br/>
+                        <input class="form-control" type="text" id="name" name="user_name" required>
+                    </div>
+
+
+                    <div class="form-group">
+                        <label for="mdp">Mot de passe :</label><br/>
+                        <input class="form-control" type="password" id="mdp" name="user_mdp" required>
+                    </div>
+
+                     <?php if (!empty($erreur)) {echo $erreur;} ?>
+                   <input class="btn btn-primary" type="submit" value="Connexion">
+
+                </form>
+
 
 
 <?php
